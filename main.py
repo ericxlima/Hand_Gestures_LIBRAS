341056c1873c714c23d8281311d5503e80aea18f
eric_lima <eric.vinlima@gmail.com>
2021-08-19 04:22:28 -0300

    refactor: modify some vars and scope

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 import mediapipe as mp
 import cv2
 
+#  Time for request
+#from time import sleep
 
 #  Utilities for Hands and Draw
 mp_drawing = mp.solutions.drawing_utils
@@ -25,7 +27,6 @@ with mp_hands.Hands(max_num_hands=1,
             print("Ignoring empty camera frame.")
             break
 
-
         #  Flipping the image horizontally and show
         image = cv2.flip(frame, 1)
 
@@ -44,39 +45,50 @@ with mp_hands.Hands(max_num_hands=1,
         key = cv2.waitKey(1)
 
         #  To exit the program
-        if key == ord('q'):
+        if key in [ord('q'), ord('Q')]:
             break
         
         #  Rendering Results in Screen
-        indexes = [0, 4, 5, 12, 20]
         if results.multi_hand_landmarks:
+            main_indexes = [0, 4, 5, 12, 20]
+
+            landmark_style = mp_drawing.DrawingSpec(color=(255, 1, 255),
+                                                    thickness=4,
+                                                    circle_radius=4)
+            connection_style = mp_drawing.DrawingSpec(color=(15, 219, 19),
+                                                      thickness=4,
+                                                      circle_radius=2)
+            
             for hand in results.multi_hand_landmarks:
                 mp_drawing.draw_landmarks(image=image,
                                           landmark_list=hand,
                                           connections=mp_hands.HAND_CONNECTIONS, 
-                                          landmark_drawing_spec=mp_drawing.DrawingSpec(color=(255, 1, 255),
-                                                                                       thickness=4,
-                                                                                       circle_radius=4),
-                                          connection_drawing_spec=mp_drawing.DrawingSpec(color=(15, 219, 19),
-                                                                                         thickness=4,
-                                                                                         circle_radius=2))
+                                          landmark_drawing_spec=landmark_style,
+                                          connection_drawing_spec=connection_style)
                 
-                #  Coloring the priciple indexes
-                for (i, landmark) in enumerate(hand.landmark):
-                    if i in indexes:
+                #  Coloring the main indexes
+                for idx, landmark in enumerate(hand.landmark):
+                    if idx in main_indexes:
                         x, y = int(landmark.x * WIDTH), int(landmark.y * HEIGHT)
-                        cv2.circle(image, (x, y), 5, (1, 12, 255), 6)
+                        cv2.circle(img=image,
+                                   center=(x, y),
+                                   radius=5,
+                                   color=(1, 12, 255),
+                                   thickness=6)
         
         #  Other operations, capturing the current moment
         if key == ord(' '):
 
-            #  Pause Video
+            #  Pause Video or sleeping
             cv2.waitKey(-1)
+            #  sleep(3)
             
             #  Captured Hands
             cap_hands = results.multi_hand_landmarks
             
-            if cap_hands:
+            if not cap_hands:
+                print('No identified hand')
+            else:
                 """ We will use only this five dots for mapping
                 0  - WRIST                  |  12  - MIDDLE_FINGER_TIP
                 4  - THUMB_TIP              |  20 - PINKY_TIP
@@ -103,9 +115,6 @@ with mp_hands.Hands(max_num_hands=1,
 
                 #  To Debug
                 print(landmarks)
-
-            else:
-                print('No identified hand')
         
         #  Show in the Screen
         cv2.imshow('Hand Gestures LIBRAS', image)
