f2a29be7924efe346b05729cda8bbabc1b1a4307
eric_lima <eric.vinlima@gmail.com>
2021-08-24 12:25:33 -0300

    docs: finalize docs in english

--- main.py
+++ main.py
@@ -135,7 +135,7 @@ with mp_hands.Hands(max_num_hands=1,
                         org=(50, axis_y),
                         fontFace=cv2.QT_STYLE_NORMAL,
                         fontScale=1,
-                        color=(0, 0, 0),
+                        color=(0, 255, 0),
                         thickness=2)
 
 
